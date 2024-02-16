a773426de0c3cbafa5c9f03e1bf71485a5753a82
Prakash <prakashverma12011@gmail.com>
2024-02-16 20:38:58 +0530

    added new feature

--- assets/java script/index.js
+++ assets/java script/index.js
@@ -2,6 +2,7 @@ function change(){
   let value = document.getElementsByName("level");
   let selected;
 
+
   for(let i = 0; i < value.length; i++){
     if(value[i].checked){
     selected = (value[i].value);
@@ -11,10 +12,14 @@ function change(){
   if(selected == 1){
     document.querySelectorAll(".sharingan")?.forEach(el => el.style.zIndex = "2")
     document.querySelectorAll(".ms")?.forEach(el => el.style.zIndex = "1")
-  }
+    document.querySelectorAll(".sharingan")?.forEach(el => el.classList.add('sharingan_rotation'))
+    document.querySelectorAll(".ms")?.forEach(el => el.classList.remove('ms_rotation'))
+    }
   if(selected == 2){
     document.querySelectorAll(".sharingan")?.forEach(el => el.style.zIndex = "1")
     document.querySelectorAll(".ms")?.forEach(el => el.style.zIndex = "2")
+    document.querySelectorAll(".ms")?.forEach(el => el.classList.add('ms_rotation'))
+    document.querySelectorAll(".sharingan")?.forEach(el => el.classList.remove('sharingan_rotation'))
   }
 
 }
